4a866178deceb7ba741673daf41551c444551d6c
Utkarsh10 <utkarshnagar2000@gmail.com>
2022-01-22 21:43:06 +0530

    Signup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Error from "./pages/404/404";
 import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
           <Route path="*" element={<Error />} />
         </Routes>
       </AppContext>
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,25 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import {Form, Button} from "react-bootstrap";
 
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div>
       <Form>
         <Form.Group className="mb-3" controlId="formEmail">
           <Form.Label>Email Address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
           <Form.Text className="text-muted"></Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formCheckBox">
           <Form.Check type="checkbox" placeholder="Check me out" />
         </Form.Group>
         <Button variant="primary" type="submit">
-          Singn Up
+          Sign Up
         </Button>
       </Form>
     </div>
